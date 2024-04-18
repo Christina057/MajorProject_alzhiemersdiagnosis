43d72ac8ead67d0aa7d87a9676b602920e4f910b
Karre Christina Prasamsha <112418919+Christina057@users.noreply.github.com>
2024-04-18 20:43:13 +0530

    Update newfile.py

--- newfile.py
+++ newfile.py
@@ -68,7 +68,7 @@ def home_page():
         class_labels = {0: 'MildDemented', 1: 'ModerateDemented', 2: 'NonDemented', 3: 'VeryMildDemented'}
 
     st.markdown(
-        "<h1 style='color: white;'>Comprehensive System for Alzheimer's Disease Diagnoses</h1>",
+        "<h1 style='color: white;  margin-bottom: 50px; margin-right: 50px;'>Comprehensive System for Alzheimer's Disease Diagnoses</h1>",
         unsafe_allow_html=True
     )
 
