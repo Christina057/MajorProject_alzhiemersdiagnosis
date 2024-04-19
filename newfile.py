17c54e64a73ad67938ea28a4e7c1272c6ce369b9
Gautami Mamidala <98585829+Gautami-2607@users.noreply.github.com>
2024-04-19 14:07:01 +0530

    Update newfile.py

--- newfile.py
+++ newfile.py
@@ -83,7 +83,7 @@ def home_page():
         unsafe_allow_html=True
     )
 
-    st.markdown("<h7 style='color: white;'>Choose an image...</h7>", unsafe_allow_html=True
+    st.markdown("<h7 style='color: white;'>Choose an image...</h7>", unsafe_allow_html=True)
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png"], key="fileUploader")
 
     if uploaded_file is not None:
